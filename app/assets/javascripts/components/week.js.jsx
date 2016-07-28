4637216d3b20a843203b36d822fbd3964ccd2f6c
Jeff Chang <jeffrey.chang2014@gmail.com>
2016-07-28 15:28:08 -0700

    Add key to week day items

--- app/assets/javascripts/components/week.js.jsx
+++ app/assets/javascripts/components/week.js.jsx
@@ -15,8 +15,9 @@ var Week = React.createClass({
       var date = day.date();
       var sameMonth = this.props.selectedMonth == day.month();
       var isCurrentDay = sameMonth && (date == moment().date());
+      var key = "day_" + i;
 
-      days.push(<WeekDay date={date} isCurrentDay={isCurrentDay} sameMonth={sameMonth} />);
+      days.push(<WeekDay date={date} isCurrentDay={isCurrentDay} key={key} sameMonth={sameMonth} />);
       day.add(1,"d");
     }
 
