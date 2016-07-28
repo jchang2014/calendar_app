dbb6d54ea7f61525e3605ed7066e4fe081c38bcc
Jeff Chang <jeffrey.chang2014@gmail.com>
2016-07-27 19:57:17 -0700

    Style calendar

--- app/assets/javascripts/components/days-of-the-week.js.jsx
+++ app/assets/javascripts/components/days-of-the-week.js.jsx
@@ -2,13 +2,13 @@ var DaysOfTheWeek = React.createClass({
   render: function() {
     return(
       <div className="days row">
-        <span className="col-xs-1 text-center">Sunday</span>
-        <span className="col-xs-1 text-center">Monday</span>
-        <span className="col-xs-1 text-center">Tuesday</span>
-        <span className="col-xs-1 text-center">Wednesday</span>
-        <span className="col-xs-1 text-center">Thursday</span>
-        <span className="col-xs-1 text-center">Friday</span>
-        <span className="col-xs-1 text-center">Saturday</span>
+        <b className="day-title col-xs-1 text-center">Sunday</b>
+        <b className="day-title col-xs-1 text-center">Monday</b>
+        <b className="day-title col-xs-1 text-center">Tuesday</b>
+        <b className="day-title col-xs-1 text-center">Wednesday</b>
+        <b className="day-title col-xs-1 text-center">Thursday</b>
+        <b className="day-title col-xs-1 text-center">Friday</b>
+        <b className="day-title col-xs-1 text-center">Saturday</b>
       </div>
     );
   }
