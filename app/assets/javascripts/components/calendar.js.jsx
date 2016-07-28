281447b882ec31f3bc9f8dbf78a3370a7b3379bf
Jeff Chang <jeffrey.chang2014@gmail.com>
2016-07-28 14:28:44 -0700

    Add new event form

--- app/assets/javascripts/components/calendar.js.jsx
+++ app/assets/javascripts/components/calendar.js.jsx
@@ -57,6 +57,23 @@ var Calendar = React.createClass({
 
         <DaysOfTheWeek />
         {weeks}
+
+        <div className="event-form row">
+          <div className="col-xs-7">
+            <h3>Create a new event</h3>
+            <form>
+              <div className="form-group col-xs-4">
+                <label>Event Title</label>
+                <input className="form-control" name="title" type="text"/>
+              </div>
+              <div className="form-group col-xs-6">
+                <label>Event Description</label>
+                <input className="form-control" name="description" type="text"/>
+              </div>
+              <button type="submit" className="btn btn-primary">Save</button>
+            </form>
+          </div>
+        </div>
       </div>
     );
   }
