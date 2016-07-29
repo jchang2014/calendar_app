2d73482902dd44cb37cc11c8120784a19c34dd06
Jeff Chang <jeffrey.chang2014@gmail.com>
2016-07-28 17:29:57 -0700

    Add index route for events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,9 @@
 class EventsController < ApplicationController
+  def index
+    events = Event.where(moment: params[:moment])
+    render json: events.as_json
+  end
+
   def create
     event = Event.new
     event.title = params[:title]
